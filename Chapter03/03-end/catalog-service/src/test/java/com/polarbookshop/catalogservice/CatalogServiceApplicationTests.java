23f51a1aea5437fb0d6a0ceba2e607e2cbb002d6
Fachrian LR <fachrianlr@gmail.com>
2023-10-28 11:56:00 +0700

    test github action

--- Chapter03/03-end/catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
+++ Chapter03/03-end/catalog-service/src/test/java/com/polarbookshop/catalogservice/CatalogServiceApplicationTests.java
@@ -17,7 +17,7 @@ class CatalogServiceApplicationTests {
 
     @Test
     void whenGetRequestWithIdThenBookReturned() {
-        var bookIsbn = "1231231230";
+        var bookIsbn = "1231231231";
         var bookToCreate = new Book(bookIsbn, "Title", "Author", 9.90);
         Book expectedBook = webTestClient
                 .post()
